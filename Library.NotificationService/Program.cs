25e9b9a4c71436abf73e1384ef1534c4e36ce1aa
Your Name <you@example.com>
2019-01-28 01:01:47 +0100

    Initial library system commit

---
+++ Library.NotificationService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Library.NotificationService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
